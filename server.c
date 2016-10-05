cfb491041fd5148dc66f5db3a03c459146472fe0
Nugroho Satriyanto <massatriya@gmail.com>
2016-10-05 21:07:02 +0700

    change server.c

--- server.c
+++ server.c
@@ -19,17 +19,25 @@ struct addrinfo hints, *info, *p;
 
 int newsockfd;
 
-void send_msg(char* msg);
+void send_msg(char* msg, int socket);
+//mengirim mesage ke suatu socket
 
 int compare_hostnames(char hostname[]);
+//mengecek apakah suatu string terdapat di array hostname,
+//mengembalikan 1 jika ada, 0 jika tidak
 
 char* temp_host_name;
 
 void print_nodes();
+//menuliskan semua node yang terhubung
 
 int j_node = 0;
 
 void get_mem();
+//menyalin data yang didapat dari system call free
+
+void do_the_things(int socket);
+//prosedur yang dilakukan setiap child fork
 
 FILE* in;
 extern FILE *popen();
@@ -42,31 +50,13 @@ char * pch;
 
 int main(int argc, char *argv[])
 {
-	int gai_result;
-	char tulis[100];
+	bzero(simpan,512);
 	char* dest, src;
-
-	char hostname[512];
-	hostname[511]='\0';
-	gethostname(hostname, 512);
-	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_CANONNAME;
-
-	if((gai_result = getaddrinfo(hostname, "http", &hints, &info)) != 0){
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(gai_result));
-		exit(0);
-	}
-
-	for(p=info;p!=NULL;p=p->ai_next){
-		temp_host_name = p->ai_canonname;
-		j_node++;
-	}
+	int pid;
+	int j_fork = 0;
 
 	int sockfd, portno;
 	socklen_t clilen;
-	char buffer[1024];
 	struct sockaddr_in serv_addr, cli_addr;
 	int n;
 	if (argc < 2) {
@@ -85,54 +75,30 @@ int main(int argc, char *argv[])
 		error("ERROR on binding");
 	listen(sockfd,5);
 	clilen = sizeof(cli_addr);
-	newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-	if (newsockfd < 0) 
-		error("ERROR on accept");
-
-	sprintf(tulis,"# munin node at %s\n",temp_host_name);
-	send_msg(tulis);
-	bzero(tulis,100);
-	do {
-		bzero(buffer,1024);
-		bzero(tulis, 100); 
-
-		n = read(newsockfd,buffer,1023);
-		if (n < 0) error("ERROR reading from socket");
-		//printf("%s\n",buffer);
-		if (strcmp(buffer,"cap\n")==0){
-			send_msg("cap multigraph dirtyconfig\n");
-		} else if (strcmp(buffer,"nodes\n")==0){
-			for(p=info;p!=NULL;p=p->ai_next){
-				temp_host_name = p->ai_canonname;
-				sprintf(tulis,"%s\n",temp_host_name);
-				send_msg(tulis);
-				bzero(tulis,100);
-			}
-			send_msg(".\n");
-		} else if (compare_hostnames(buffer)==1) {
-			send_msg("memory\n");
-		} else if (strcmp(buffer,"version\n")==0){
-			sprintf(tulis,"aaa node on %s version: 8.48\n",temp_host_name);
-			send_msg(tulis);
-		} else if (strcmp(buffer,"fetch memory\n") == 0){
-			get_mem();
-			sprintf(tulis,"used.value %sfree.value %s",mem_used,mem_free);
-			send_msg(tulis);
-			bzero(tulis,100);
-		} else if (strcmp(buffer,"config memory\n")==0){
-			send_msg("graph_args --base 1024 -l 0 --upper-limit");
-		} else if (strncmp(buffer,"quit\n",5)!=0) {
-			send_msg("# Unknown command. Try cap, list, nodes, config, fetch, version or quit\n");
+	do{
+		newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+		if (newsockfd < 0) 
+			error("ERROR on accept");
+		pid = fork();
+		if (pid < 0)
+			error("ERROR on fork");
+		else if (pid == 0){
+			//client process
+			close(sockfd);
+			do_the_things(newsockfd);
+			exit(0);
+		} else {
+			close(newsockfd);
+			j_fork--;
 		}
-		if (n < 0) error("ERROR writing to socket");
-	} while (strncmp(buffer,"quit\n", 5)!=0);
-	close(newsockfd);
-	close(sockfd);
+		j_fork++;
+	} while (1);
+	
 	return 0; 
 }
 
-void send_msg(char* msg){
-	write(newsockfd,msg,strlen(msg));
+void send_msg(char* msg, int socket){
+	write(socket,msg,strlen(msg));
 }
 
 
@@ -166,4 +132,109 @@ void get_mem(){
 	sprintf(mem_used,"%s\n",pch);
 	pch = strtok(NULL," ");
 	sprintf(mem_free,"%s\n",pch);
+}
+
+void do_the_things(int socket){
+	int n;
+	char tulis[100];
+	int gai_result;
+	char buffer[512];
+	char hostname[512];
+	hostname[511]='\0';
+	char * cmd;
+	gethostname(hostname, 512);
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_CANONNAME;
+
+	if((gai_result = getaddrinfo(hostname, "http", &hints, &info)) != 0){
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(gai_result));
+		exit(0);
+	}
+
+	for(p=info;p!=NULL;p=p->ai_next){
+		temp_host_name = p->ai_canonname;
+		j_node++;
+	}
+
+	for(p=info;p!=NULL;p=p->ai_next){
+		temp_host_name = p->ai_canonname;
+	}
+
+	sprintf(tulis,"# munin node at %s\n",temp_host_name);
+	send_msg(tulis,socket);
+	bzero(tulis,100);
+	do {
+		bzero(buffer,512);
+		bzero(tulis, 100); 
+		n = read(socket,buffer,511);
+		cmd = strtok(buffer," ");
+		if (n < 0) error("ERROR reading from socket");
+		//printf("%s\n",buffer);
+		if (strcmp(cmd,"cap\n")==0){
+			send_msg("cap multigraph dirtyconfig\n",socket);
+		} else if (strcmp(cmd,"nodes\n")==0){
+			gethostname(hostname, 512);
+			memset(&hints, 0, sizeof hints);
+			hints.ai_family = AF_UNSPEC;
+			hints.ai_socktype = SOCK_STREAM;
+			hints.ai_flags = AI_CANONNAME;
+
+			if((gai_result = getaddrinfo(hostname, "http", &hints, &info)) != 0){
+				fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(gai_result));
+				exit(0);
+			}
+
+			for(p=info;p!=NULL;p=p->ai_next){
+				temp_host_name = p->ai_canonname;
+				j_node++;
+			}
+
+			for(p=info;p!=NULL;p=p->ai_next){
+				temp_host_name = p->ai_canonname;
+				sprintf(tulis,"%s\n",temp_host_name);
+				send_msg(tulis,socket);
+				bzero(tulis,100);
+			}
+			send_msg(".\n",socket);
+		} else if (strcmp(cmd,"list")==0) {
+			cmd = strtok(NULL," ");
+			if (compare_hostnames(cmd)==1){
+				send_msg("memory\n",socket);
+			}
+		} else if (strcmp(cmd,"version\n")==0){
+			sprintf(tulis,"aaa node on %s version: 8.48\n",temp_host_name);
+			send_msg(tulis,socket);
+		} else if (strcmp(cmd,"fetch") == 0){
+			cmd = strtok(NULL," ");
+			if (strcmp(cmd,"memory\n")==0){
+				get_mem();
+				sprintf(tulis,"used.value %sfree.value %s",mem_used,mem_free);
+				send_msg(tulis,socket);
+			}
+		} else if (strcmp(buffer,"config")==0){
+			cmd = strtok(NULL," ");
+			if (strcmp(cmd,"memory\n")==0){
+				get_mem();
+				sprintf(tulis,"graph_args --base 1024 -l 0 --upper-limit %s",mem_total);
+				send_msg(tulis,socket);
+				send_msg("graph_vlabel Bytes\n",socket);
+				send_msg("graph_title Memory usage\n",socket);
+				send_msg("graph_category system\n",socket);
+				send_msg("graph_info This graph shows this machine memory.\n",socket);
+				send_msg("graph_order used free\n",socket);
+				send_msg("used.label used\n",socket);
+				send_msg("used.draw STACK\n",socket);
+				send_msg("used.info Used memory.\n", socket);
+				send_msg("free.label free\n", socket);
+				send_msg("free.draw STACK\n", socket);
+				send_msg("free.info Free memory.\n", socket);
+				send_msg(".\n",socket);
+			}
+		} else if (strncmp(cmd,"quit\n",5)!=0) {
+			send_msg("# Unknown command. Try cap, list, nodes, config, fetch, version or quit\n",socket);
+		}
+		if (n < 0) error("ERROR writing to socket");
+	} while (strncmp(cmd,"quit\n", 5)!=0);
 }
